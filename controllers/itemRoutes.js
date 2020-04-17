622fcf2d67fbaf23266ceb4642e3cad1b2122e0f
Jeremy Hilado <jhilado89@gmail.com>
2020-04-17 13:58:19 -0700

    setup controllers

--- controllers/itemRoutes.js
+++ controllers/itemRoutes.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const 
+const itemsController = require('./items')
+
+// Get all items
+router.get('/', itemsController.getAllItems)
+
+// Get item by _id
+router.get('/:id', itemsController.getItemById)
+
+// Create item
+router.post('/', itemsController.createItem)
+
+// Update item
+router.put('/', itemsController.updateItem)
+
+// Delete item
+router.delete('/', itemsController.deleteItem)
+
+module.exports = router
--- controllers/items.js
+++ controllers/items.js
@@ -9,5 +9,39 @@ const getAllItems = (req, res) => {
 
 // Controller to get item by _id
 const getItemById = (req, res) => {
-    Item.
+    Item.findById(req.params.id).then(item => {
+        res.json(item)
+    })
+}
+
+// Controller to create an item
+const createItem = (req, res) => {
+    Item.create(req.body).then(item => {
+        res.json(item)
+    })
+}
+
+// Controller to update an item
+const updateItem = (req, res) => {
+    Item.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(item => {
+        res.json(item)
+    })
+}
+
+// Controller to delete an item
+const deleteItem = (req, res) => {
+    Item.findByIdAndDelete(req.params.id).then(author => {
+        res.json({
+            "status": "success",
+            "msg": "author deleted"
+        })
+    })
+}
+
+module.exports = {
+    getAllItems,
+    getItemById,
+    createItem,
+    updateItem,
+    deleteItem
 }
