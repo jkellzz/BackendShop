605a8f7580138399b3a38f992ac63ec013a9f78e
Bernd van der Lande <berndvanderlande@Bernds-MacBook-Air.local>
2020-04-21 16:06:54 -0400

    finish

--- model/reviewModel.js
+++ model/reviewModel.js
@@ -2,8 +2,6 @@ const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
 
 const ReviewSchema = Schema({
-    "item": String,
-    "price": Number,
     "date": {type: Date, default: Date.now},
     "rating": Number,
     "reviews": String
